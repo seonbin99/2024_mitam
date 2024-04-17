fc9967cabb92d8db963867863ac051529c531d82
seonbin99 <tst7@naver.com>
2024-04-17 16:27:09 +0900

    script로 zindex랑 opacity 지정

--- script/action.js
+++ script/action.js
@@ -9,3 +9,5 @@ $('header .centerbox nav .ham').on('click',function(){
     $(this).toggleClass('on');
     $('header .centerbox nav .navbg').stop().fadeToggle();
 })
+$('#visual .textbox h2').css({zIndex:20, opacity:1})
+$('#visual .textbox .deco ').css({zIndex:1})
