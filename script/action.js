9c047dd330c861226021c564cc20f3a3caac31bb
seonbin99 <tst7@naver.com>
2024-04-18 15:55:33 +0900

    동그라미 애니메이션 제거

--- script/action.js
+++ script/action.js
@@ -9,6 +9,5 @@ $('header .centerbox nav .ham').on('click',function(){
     $(this).toggleClass('on');
     $('header .centerbox nav .navbg').stop().fadeToggle();
 })
-$('#visual .textbox').css({zIndex:20})
-$('#visual .textbox h2').css({zIndex:20})
-$('#visual .deco ').css({zIndex:1})
+$('#visual .textbox').css({zIndex:20});
+$('#visual .textbox h2').css({zIndex:20});
